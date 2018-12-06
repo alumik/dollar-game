516c2820dd72d84c5a30c3b95e4c52f7d8b0f27f
Zhong Zhenyu <nczzy1997@gmail.com>
2018-12-06 15:33:25 +0800

    首次提交

---
+++ js/cell.js
@@ -0,0 +1,58 @@
+class Cell {
+    constructor(x, y, index) {
+        this.index = index
+        this.size = 40
+        this.pos = createVector(x, y)
+        this.num = 0
+        this.neighbors = []
+        this.active = false
+        this.mouse_in = false
+        this.show_num = false
+        this.new_input = true
+    }
+
+    show() {
+        noStroke()
+        fill(0)
+        ellipse(this.pos.x, this.pos.y, this.size, this.size)
+
+        if (this.show_num) {
+            textAlign(CENTER, CENTER)
+            textSize(20)
+            fill(255)
+            text(this.num, this.pos.x, this.pos.y)
+        }
+
+        if (this.active) {
+            stroke(0, 255, 0)
+            strokeWeight(5)
+            noFill()
+            ellipse(this.pos.x, this.pos.y, this.size, this.size)
+        }
+
+        if (this.mouse_in) {
+            stroke(255, 0, 0)
+            strokeWeight(3)
+            noFill()
+            ellipse(this.pos.x, this.pos.y, this.size, this.size)
+        }
+    }
+
+    flipState() {
+        this.active = !this.active
+    }
+
+    showLink() {
+        for (let neighor of this.neighbors) {
+            if (this.index < neighor.index) {
+                Cell.showLink(this, neighor)
+            }
+        }
+    }
+
+    static showLink(cell_a, cell_b) {
+        stroke(0)
+        strokeWeight(5)
+        line(cell_a.pos.x, cell_a.pos.y, cell_b.pos.x, cell_b.pos.y)
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,198 @@
+const BEFORE_GAME = 0
+const IN_GAME = 1
+
+let g_game_state = BEFORE_GAME
+let g_edges_count = 0
+let g_cell_index = 0
+let c_active_cell
+let c_drag_cell
+let c_press_cell
+let c_cells = []
+
+function setup() {
+    createCanvas(1024, 768)
+    c_drag_cell = new Cell(0, 0, null)
+}
+
+function draw() {
+    background(180)
+    mouseDragHandler()
+    for (let cell of c_cells) {
+        cell.showLink()
+        cell.show()
+    }
+}
+
+function mousePressed() {
+    c_press_cell = null
+    if (g_game_state === BEFORE_GAME) {
+        if (c_active_cell) {
+            c_active_cell.flipState()
+            c_active_cell.new_input = true
+        }
+        c_active_cell = null
+        let in_cell = getInCell()
+        if (mouseButton === LEFT) {
+            if (in_cell) {
+                c_press_cell = in_cell
+            } else if (!isNear()) {
+                let new_cell = new Cell(mouseX, mouseY, g_cell_index++)
+                c_cells.push(new_cell)
+                c_press_cell = new_cell
+            }
+        } else if (mouseButton === RIGHT) {
+            c_press_cell = in_cell
+        }
+    } else if (g_game_state === IN_GAME) {
+        let in_cell = getInCell()
+        if (in_cell) {
+            c_press_cell = in_cell
+        }
+    }
+}
+
+function mouseReleased() {
+    if (g_game_state === BEFORE_GAME) {
+        if (mouseButton === LEFT) {
+            for (let cell of c_cells) {
+                if (dist(mouseX, mouseY, cell.pos.x, cell.pos.y) <= cell.size / 2) {
+                    if (cell === c_press_cell) {
+                        cell.flipState()
+                        c_active_cell = cell
+                    } else {
+                        if (c_press_cell.neighbors.indexOf(cell) === -1) {
+                            c_press_cell.neighbors.push(cell)
+                            cell.neighbors.push(c_press_cell)
+                            g_edges_count++
+                        }
+                    }
+                    return
+                }
+            }
+            if (!isNear()) {
+                let new_cell = new Cell(mouseX, mouseY, g_cell_index++)
+                c_cells.push(new_cell)
+                c_press_cell.neighbors.push(new_cell)
+                new_cell.neighbors.push(c_press_cell)
+                new_cell.flipState()
+                c_active_cell = new_cell
+                g_edges_count++
+            }
+        }
+    } else if (g_game_state === IN_GAME) {
+        let in_cell = getInCell()
+        if (in_cell && in_cell === c_press_cell) {
+            if (mouseButton === LEFT) {
+                for (let neighbor of in_cell.neighbors) {
+                    in_cell.num--
+                    neighbor.num++
+                }
+            } else if (mouseButton === RIGHT) {
+                for (let neighbor of in_cell.neighbors) {
+                    in_cell.num++
+                    neighbor.num--
+                }
+            }
+            checkWin()
+        }
+    }
+    return false
+}
+
+function mouseDragHandler() {
+    if (g_game_state === BEFORE_GAME && mouseIsPressed && c_press_cell) {
+        if (mouseButton === LEFT) {
+            if (dist(mouseX, mouseY, c_press_cell.pos.x, c_press_cell.pos.y) > c_press_cell.size / 2) {
+                c_drag_cell.pos.x = mouseX
+                c_drag_cell.pos.y = mouseY
+                c_drag_cell.show()
+                Cell.showLink(c_press_cell, c_drag_cell)
+            }
+        } else if (mouseButton === RIGHT) {
+            c_press_cell.pos.x = mouseX
+            c_press_cell.pos.y = mouseY
+        }
+    }
+}
+
+function getInCell() {
+    let in_cell
+    for (let cell of c_cells) {
+        if (dist(mouseX, mouseY, cell.pos.x, cell.pos.y) <= cell.size / 2) {
+            in_cell = cell
+            cell.mouse_in = true
+        } else {
+            cell.mouse_in = false
+        }
+    }
+    return in_cell
+}
+
+function isNear() {
+    for (let cell of c_cells) {
+        if (dist(mouseX, mouseY, cell.pos.x, cell.pos.y) <= cell.size) {
+            return true
+        }
+    }
+    return false
+}
+
+function mouseMoved() {
+    getInCell()
+}
+
+function checkGame() {
+    let sum = 0
+    for (let cell of c_cells) {
+        sum += cell.num
+        if (!cell.show_num) {
+            return false
+        }
+    }
+    console.log(g_edges_count - c_cells.length + 1 - sum)
+    return g_edges_count - c_cells.length + 1 <= sum
+}
+
+function checkWin() {
+    let win = true
+    for (let cell of c_cells) {
+        if (cell.num < 0) {
+            win = false
+        }
+    }
+    if (win) {
+        console.log('游戏成功！')
+    }
+}
+
+function keyPressed() {
+    if (c_active_cell && g_game_state === BEFORE_GAME) {
+        if (key >= '0' && key <= '9') {
+            if (c_active_cell.new_input) {
+                c_active_cell.num = parseInt(key)
+                c_active_cell.new_input = false
+            } else {
+                c_active_cell.num = parseInt('' + c_active_cell.num + key)
+            }
+            c_active_cell.show_num = true
+        } else if (key === '-') {
+            c_active_cell.num = -c_active_cell.num
+        } else if (key === 'Delete') {
+            c_cells.splice(c_cells.indexOf(c_active_cell), 1)
+            for (let neighbor of c_active_cell.neighbors) {
+                neighbor.neighbors.splice(neighbor.neighbors.indexOf(c_active_cell), 1)
+                g_edges_count--
+            }
+        }
+    }
+    if (key === 'Enter') {
+        if (checkGame()) {
+            if (c_active_cell) {
+                c_active_cell.flipState()
+            }
+            g_game_state = IN_GAME
+        } else {
+            alert('游戏参数不正确!')
+        }
+    }
+}
